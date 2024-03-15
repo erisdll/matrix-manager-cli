2ce8a8e7c0a468b70ba424c16de6644da04f3e1b
erisdll <72031082+erisdll@users.noreply.github.com>
2024-03-14 22:22:23 -0300

    began impl of operations submenu

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,10 @@ public class Main {
 
             switch (choice) {
 
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+
                 case 1:
                     createMatrixCase(matrixManager, scanner);
                     break;
@@ -27,19 +31,13 @@ public class Main {
                     break;
 
                 case 4:
-                    selectMatrixCase(matrixManager);
-                    break;
-
-                case 5:
-                    System.out.println("Exiting...");
+                    selectMatrixToOperateCase(matrixManager, scanner);
                     break;
 
                 default:
                     System.out.println("Invalid choice!");
-                    break;
             }
-        } while (choice != 6);
-        scanner.close();
+        } while (choice != 0);
     }
 
     private static void printMenu() {
@@ -47,43 +45,75 @@ public class Main {
         System.out.println("Type 2 to list exiting matrices");
         System.out.println("Type 3 to populate a matrix");
         System.out.println("Type 4 to select a matrix to operate on");
-        System.out.println("Type 5 to exit program");
+        System.out.println("Type 0 to exit program");
     }
 
     private static void createMatrixCase(MatrixManager matrixManager, Scanner scanner) {
         Matrix matrix = new Matrix(getMatrixSizeFromConsole(scanner));
-        matrixManager.addMatrix(matrix);
+        matrixManager.addMatrixToList(matrix);
         System.out.println("Matrix created");
-        populateMatrixCase(matrixManager, scanner);
+        populateMatrix(matrix, scanner);
     }
 
     private static void populateMatrixCase(MatrixManager matrixManager, Scanner scanner) {
         listMatricesCase(matrixManager);
         System.out.println("Choose matrix to populate:");
         int matrixIndex = (scanner.nextInt() - 1);
-        Matrix matrix = matrixManager.getMatrixByIndex(matrixIndex);
+        populateMatrix(matrixManager.getMatrixByIndex(matrixIndex), scanner);
+    }
 
+    private static void populateMatrix(Matrix matrix, Scanner scanner) {
         for (int row = 0; row < matrix.getRowCount(); row++) {
             for (int column = 0; column < matrix.getColumnCount(); column++) {
-                System.out.println("Insert value for position (" + (row + 1) + ", " + (column + 1) + "):");
+                System.out.println("Insert value for position (" + (row + 1 + ", " + (column + 1) + "):"));
                 int value = getValueFromConsole(scanner);
                 matrix.setElementValue(new SimpleMatrixPosition(row, column), value);
             }
         }
     }
 
+    private static void selectMatrixToOperateCase(MatrixManager matrixManager, Scanner scanner) {
+        listMatricesCase(matrixManager);
+        int choice;
+        do {
+            printSelectedMatrixSubmenu();
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+
+                case 1:
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (choice != 0);
+    }
+
     private static void listMatricesCase(MatrixManager matrixManager) {
         System.out.println("List of matrices:");
         for (int index = 0; index < matrixManager.getMatrixCount(); index++) {
-            System.out.println(" ");
+
+            System.out.println("\nMatrix n." + (index + 1) + ":");
             Matrix matrix = matrixManager.getMatrixByIndex(index);
             matrix.printMatrix();
         }
         System.out.println(" ");
     }
 
-    private static void selectMatrixCase(MatrixManager matrixManager) {
-
+    private static void printSelectedMatrixSubmenu() {
+        System.out.println("Type 1 to repopulate matrix");
+        System.out.println("Type 2 to change an element's value");
+        System.out.println("Type 3 to find the determinant");
+        System.out.println("Type 4 to invert the matrix");
+        System.out.println("Type 5 to transpose matrix");
+        System.out.println("Type 6 to add another matrix");
+        System.out.println("Type 7 to subtract another matrix");
+        System.out.println("Type 8 to multiply by scalar");
+        System.out.println("Type 9 to multiply by another matrix");
+        System.out.println("Type 10 to divide by scalar");
+        System.out.println("Type 11 to divide by another matrix");
+        System.out.println("Type 0 to return to main menu");
     }
 
     private static MatrixSize getMatrixSizeFromConsole(Scanner scanner) {
