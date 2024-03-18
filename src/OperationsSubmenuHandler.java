7fd205efe6adc94225491ccba0dade695a8447d4
erisdll <72031082+erisdll@users.noreply.github.com>
2024-03-18 16:29:00 -0300

    refactored constructor, created add/subtract methods

--- src/OperationsSubmenuHandler.java
+++ src/OperationsSubmenuHandler.java
@@ -1,36 +1,49 @@
 import java.util.Scanner;
 
 public class OperationsSubmenuHandler {
-    public void startOperationsSubMenu(Matrix matrix, Scanner scanner) {
+
+    private final Scanner scanner;
+    private final MatrixManager matrixManager;
+
+    public OperationsSubmenuHandler(MatrixManager matrixManager, Scanner scanner) {
+        this.scanner = scanner;
+        this.matrixManager = matrixManager;
+    }
+    public void startOperationsSubmenuOnMatrix(Matrix matrix) {
         int choice;
         do {
             printOperationsSubmenu();
             choice = scanner.nextInt();
-            handleOperationsSubMenuChoice(choice, matrix, scanner);
+            handleOperationsSubmenuChoice(choice, matrix);
         } while (choice != 0);
     }
 
     private static void printOperationsSubmenu() {
         System.out.println("Type 1 to repopulate matrix");
         System.out.println("Type 2 to change an element's value");
-        System.out.println("Type 3 to find determinant");
-        System.out.println("Type 4 to invert matrix");
-        System.out.println("Type 5 to transpose matrix");
-        System.out.println("Type 6 to add to another matrix");
-        System.out.println("Type 7 to subtract another matrix");
-        System.out.println("Type 8 to multiply matrix");
-        System.out.println("Type 9 to divide matrix");
+        System.out.println("Type 3 to add to another matrix");
+        System.out.println("Type 4 to subtract another matrix");
+        System.out.println("Type 5 to multiply matrix");
+        System.out.println("Type 6 to divide matrix");
         System.out.println("Type 0 to return to main menu");
     }
 
-    private void handleOperationsSubMenuChoice(int choice, Matrix matrix, Scanner scanner) {
+    private void handleOperationsSubmenuChoice(int choice, Matrix matrix) {
         switch (choice) {
             case 1:
-                repopulateMatrixOption(matrix, scanner);
+                repopulateMatrixOption(matrix);
                 break;
 
             case 2:
-                changeElementValueOption(matrix, scanner);
+                changeOneElementValueOption(matrix);
+                break;
+
+            case 3:
+                addAnotherMatrixOption(matrix);
+                break;
+
+            case 4:
+                subtractAnotherMatrixOption(matrix);
                 break;
 
             case 0:
@@ -42,9 +55,9 @@ public class OperationsSubmenuHandler {
         }
     }
 
-    private void repopulateMatrixOption(Matrix matrix, Scanner scanner) {
-        for (int row = 0; row < matrix.getRowCount(); row++) {
-            for (int column = 0; column < matrix.getColumnCount(); column++) {
+    private void repopulateMatrixOption(Matrix matrix) {
+        for (int row = 0; row < matrix.getRows(); row++) {
+            for (int column = 0; column < matrix.getColumns(); column++) {
                 System.out.println("Insert value for position (" + row + ", " + column + ").");
                 matrix.setElementValue(new SimpleMatrixPosition(row, column), scanner.nextInt());
             }
@@ -53,13 +66,70 @@ public class OperationsSubmenuHandler {
         matrix.printMatrix();
     }
 
-    private void changeElementValueOption(Matrix matrix, Scanner scanner) {
-        matrix.setElementValue(getPositionFromConsole(scanner), getValueFromConsole(scanner));
+    private void changeOneElementValueOption(Matrix matrix) {
+        matrix.setElementValue(getPositionFromConsole(), getValueFromConsole());
         matrix.printMatrix();
         System.out.println(" ");
     }
 
-    private static MatrixPosition getPositionFromConsole(Scanner scanner) {
+    private void addAnotherMatrixOption(Matrix matrix) {
+        Matrix secondMatrix = selectSecondMatrixForOperation("addition");
+
+        if ( matrix.getRows() != secondMatrix.getRows() || matrix.getColumns() != secondMatrix.getColumns()) {
+            System.out.println("Cannot add matrices of different dimensions!");
+        } else {
+            for (int row = 0; row < matrix.getRows(); row++) {
+                for (int column = 0; column < matrix.getColumns(); column++) {
+                    MatrixPosition pos = new SimpleMatrixPosition(row, column);
+                    matrix.setElementValue(pos, matrix.getElementValue(pos) + secondMatrix.getElementValue(pos));
+                }
+            }
+            System.out.println("Result:");
+            matrix.printMatrix();
+        }
+    }
+
+    private void subtractAnotherMatrixOption(Matrix matrix) {
+        Matrix secondMatrix = selectSecondMatrixForOperation("subtraction");
+
+        if ( matrix.getRows() != secondMatrix.getRows() || matrix.getColumns() != secondMatrix.getColumns()) {
+            System.out.println("Cannot subtract matrices of different dimensions!");
+        } else {
+            for (int row = 0; row < matrix.getRows(); row++) {
+                for (int column = 0; column < matrix.getColumns(); column++) {
+                    MatrixPosition pos = new SimpleMatrixPosition(row, column);
+                    matrix.setElementValue(pos, matrix.getElementValue(pos) - secondMatrix.getElementValue(pos));
+                }
+            }
+            System.out.println("Result:");
+            matrix.printMatrix();
+        }
+    }
+
+    private Matrix selectSecondMatrixForOperation(String operation) {
+
+        String operationString = switch (operation) {
+            case "addition" -> "add to:";
+            case "subtraction" -> "subtract:";
+            case "multiplication" -> "multiply by:";
+            case "division" -> "divide by";
+            default -> null;
+        };
+
+        System.out.println("Select Matrix to " + operationString);
+        System.out.println("List of matrices:");
+
+        int index = 0;
+        for (Matrix matrices : matrixManager.listAllMatrices()) {
+            System.out.println("\nMatrix n." + (index + 1) + ":");
+            matrices.printMatrix();
+            index++;
+        }
+
+        return matrixManager.selectMatrix(scanner.nextInt() - 1 );
+    }
+
+    private MatrixPosition getPositionFromConsole() {
         System.out.println("Insert desired position:");
         System.out.println("Row:");
         int row = scanner.nextInt() - 1;
@@ -68,10 +138,9 @@ public class OperationsSubmenuHandler {
         return new SimpleMatrixPosition(row, column);
     }
 
-    private static Integer getValueFromConsole(Scanner scanner) {
+    private Integer getValueFromConsole() {
         System.out.println("Insert desired value:");
         return scanner.nextInt();
-
     }
 }
 
